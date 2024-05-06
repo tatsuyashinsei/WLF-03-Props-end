c8e601c429d5f6297d2bfad2a30c3b0d31d545cb
tatsuyashinsei <145214721+tatsuyashinsei@users.noreply.github.com>
2024-05-06 22:05:39 +0900

    Initial commit

---
+++ src/components/PageTitle.tsx
@@ -0,0 +1,17 @@
+// PageTitle.tsx
+
+// import { useState } from 'react';
+// import reactLogo from './assets/react.svg';
+// import viteLogo from '/vite.svg';
+// import './App.css';
+import * as React from 'react';
+
+// function PageTitle(props) {
+//   return <h1>Hello {props.title} </h1>;
+// }
+function PageTitle({ title }) {
+  return <h1>Hello {title} </h1>;
+}
+
+// export default App;
+export default PageTitle;
